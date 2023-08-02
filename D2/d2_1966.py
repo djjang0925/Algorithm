73480f9ba51e1c288ca1b669faebc6b9987e3c59
뚱이 <djjang0925@gmail.com>
2023-08-02 20:57:17 +0900

    230802

--- D2/d2_1966.py
+++ D2/d2_1966.py
@@ -2,7 +2,20 @@ test_case = int(input())
 
 for case in range(1, test_case + 1):
     n = int(input())
-    numbers = list(map(int, input().split()))
-    numbers.sort()
+    test_list = list(map(int, input().split()))
+    max_index = max(test_list) + 1
+    count = {num: 0 for num in range(max_index)}
+    res = [0] * len(test_list)
 
-    print(f'#{case}', *numbers)
+    for num in test_list:
+        count[num] += 1
+
+    for num in range(1, max_index):
+        count[num] += count[num - 1]
+
+    for num in test_list:
+        res[count[num] - 1] = num
+        count[num] -= 1
+
+    print(f'#{case}', end=' ')
+    print(*res)
