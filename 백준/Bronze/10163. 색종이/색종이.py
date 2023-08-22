08127c2c76875a2b62653c30f8183d3aaeec2546
뚱이 <88839216+djjang0925@users.noreply.github.com>
2023-08-22 21:01:45 +0900

    Update 색종이.py

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -2,22 +2,24 @@ import sys
 input = sys.stdin.readline
 
 
+# 2중 for문 > 슬라이싱 (시간복잡도)
 def color(root, goal, paint):
-    for i in range(root[0], goal[0]):
-        for j in range(root[1], goal[1]):
-            if graph[i][j] != 0:
-                colors[graph[i][j]] -= 1
-            graph[i][j] = paint
-            colors[paint] += 1
+    for i in range(root[0], root[0] + goal[0]):
+        graph[i][root[1]:root[1] + goal[1]] = [paint] * goal[1]
 
 
 n = int(input())
-graph = [[0] * 1002 for _ in range(1002)]
+graph = [[0] * 1001 for _ in range(1001)]
 colors = {i: 0 for i in range(1, n + 1)}
 
-for i in range(n):
+for i in range(1, n + 1):
     a, b, c, d = map(int, input().rstrip().split())
-    color([a, b], [a + c, b + d], i + 1)
+    color([a, b], [c, d], i)
+
+for i in range(1001):
+    for j in range(1001):
+        if graph[i][j] != 0:
+            colors[graph[i][j]] += 1
 
 for i in colors:
     print(colors[i])
