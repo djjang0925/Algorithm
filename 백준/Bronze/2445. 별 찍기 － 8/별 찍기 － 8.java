565976a5c65883b07ff43112817c17123d24b162
뚱이 <88839216+djjang0925@users.noreply.github.com>
2023-09-06 00:10:30 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 652 ms, Memory: 20340 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i=1; i<=n; i++) {
+			
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			
+			for (int j=1; j<=2*(n-i); j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			
+			System.out.println();
+			
+		}
+		
+		for (int i=n-1; i>=1; i--) {
+			
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			
+			for (int j=1; j<=2*(n-i); j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			
+			System.out.println();
+			
+		}
+	}
+}
