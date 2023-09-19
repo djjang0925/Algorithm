557834bf8287c236e6b9363303ecb5513e56e70d
뚱이 <88839216+djjang0925@users.noreply.github.com>
2023-09-19 16:06:40 +0900

    [Gold IV] Title: 키 순서, Time: 596 ms, Memory: 121236 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.py
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -1,25 +1,44 @@
 import sys
 input = sys.stdin.readline
 
+
+def goal(x):
+    stack1 = [x]
+    stack2 = [x]
+    visited = [0] * (n + 1)
+    visited[x] = 1
+
+    while stack1:
+        now = stack1.pop()
+
+        for i in range(len(graph_2[now])):
+            if visited[graph_2[now][i]] == 0:
+                visited[graph_2[now][i]] = 1
+                stack1.append(graph_2[now][i])
+
+    while stack2:
+        now = stack2.pop()
+
+        for i in range(len(graph_1[now])):
+            if visited[graph_1[now][i]] == 0:
+                visited[graph_1[now][i]] = 1
+                stack2.append(graph_1[now][i])
+
+    return visited.count(1)
+
+
 n, m = map(int, input().rstrip().split())
-graph = [[0] * n for _ in range(n)]
+graph_1 = {i: [] for i in range(1, n + 1)}
+graph_2 = {i: [] for i in range(1, n + 1)}
 res = 0
 
 for i in range(m):
-    a, b = map(int, input().rstrip().split())
-    graph[a - 1][b - 1] = 1
+    a, b = map(int, input().split())
+    graph_1[a].append(b)
+    graph_2[b].append(a)
 
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            if graph[i][k] == 1 and graph[k][j] == 1:
-                graph[i][j] = 1
-
-for i in range(n):
-    s = sum(graph[i])
-    l = sum([1 if graph[j][i] == 1 else 0 for j in range(n)])
-
-    if s + l == n - 1:
+for i in range(1, n + 1):
+    if goal(i) == n:
         res += 1
 
 print(res)
