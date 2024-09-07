6faf76e5ce8760e0c97e0d991e826578edbc18b7
뚱이 <88839216+djjang0925@users.noreply.github.com>
2024-09-07 13:44:39 +0900

    [Silver II] Title: DNA 비밀번호, Time: 172 ms, Memory: 22512 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,12 +1,11 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int S, P;
-    static char[] Str;
-    static int[] Key = new int[4];
-    static int[] CurrentCount = new int[4];
-    static int Res = 0;
+    static char[] Arr;
+    static int[] Key, Used;
+    static int Cnt = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,54 +13,66 @@ public class Main {
 
         S = Integer.parseInt(st.nextToken());
         P = Integer.parseInt(st.nextToken());
+        Key = new int[4];
+        Used = new int[4];
 
-        Str = br.readLine().toCharArray();
+        Arr = br.readLine().toCharArray();
 
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < 4; i++) {
             Key[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < P; i++) {
-            addChar(Str[i]);
+            AddChar(Arr[i]);
         }
 
-        if (isValid()) {
-            Res++;
+        int s = 0, e = P - 1;
+
+        while (true) {
+            IsValid();
+
+            if (e == S - 1) break;
+
+            AddChar(Arr[++e]);
+            RemoveChar(Arr[s++]);
         }
 
-        for (int i = P; i < S; i++) {
-            addChar(Str[i]);
-            removeChar(Str[i - P]);
+        System.out.println(Cnt);
+    }
 
-            if (isValid()) {
-                Res++;
-            }
+    private static void RemoveChar(char c) {
+        if (c == 'A') {
+            Used[0] -= 1;
+        } else if (c == 'C') {
+            Used[1] -= 1;
+        } else if (c == 'G') {
+            Used[2] -= 1;
+        } else if (c == 'T') {
+            Used[3] -= 1;
         }
-
-        System.out.println(Res);
     }
 
-    static void addChar(char c) {
-        if (c == 'A') CurrentCount[0]++;
-        else if (c == 'C') CurrentCount[1]++;
-        else if (c == 'G') CurrentCount[2]++;
-        else if (c == 'T') CurrentCount[3]++;
-    }
-
-    static void removeChar(char c) {
-        if (c == 'A') CurrentCount[0]--;
-        else if (c == 'C') CurrentCount[1]--;
-        else if (c == 'G') CurrentCount[2]--;
-        else if (c == 'T') CurrentCount[3]--;
-    }
-
-    static boolean isValid() {
+    private static void IsValid() {
         for (int i = 0; i < 4; i++) {
-            if (CurrentCount[i] < Key[i]) {
-                return false;
+            if (Used[i] < Key[i]) {
+                return;
             }
         }
-        return true;
+
+        Cnt++;
+    }
+
+    private static void AddChar(char c) {
+        if (c == 'A') {
+            Used[0] += 1;
+        } else if (c == 'C') {
+            Used[1] += 1;
+        } else if (c == 'G') {
+            Used[2] += 1;
+        } else if (c == 'T') {
+            Used[3] += 1;
+        }
     }
 }
