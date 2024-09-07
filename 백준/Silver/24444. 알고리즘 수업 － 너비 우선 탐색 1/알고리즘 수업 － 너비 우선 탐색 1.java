5b6d8473a08ddcf00bd9c81a775b32eba7b9b7b2
뚱이 <88839216+djjang0925@users.noreply.github.com>
2024-09-07 12:11:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1920 ms, Memory: 118156 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -4,8 +4,7 @@ import java.io.*;
 public class Main {
     static int N, M, R;
     static List<Integer>[] Graph;
-    static int[] Order;
-    static int Cnt = 1;
+    static int[] Visited, Order;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,15 +14,17 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
 
-        Order = new int[N];
         Graph = new ArrayList[N + 1];
+        Visited = new int[N];
+        Order = new int[N];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i <= N; i++) {
             Graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
+
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
@@ -31,8 +32,8 @@ public class Main {
             Graph[y].add(x);
         }
 
-        for (int i = 1; i <= N; i++) {
-            Graph[i].sort(Comparator.naturalOrder());
+        for (int i = 0; i < N + 1; i++) {
+            Collections.sort(Graph[i]);
         }
 
         Bfs(R);
@@ -43,14 +44,16 @@ public class Main {
     }
 
     private static void Bfs(int r) {
+        int cnt = 1;
         Deque<Integer> deque = new ArrayDeque<>();
         deque.addLast(r);
 
         while (!deque.isEmpty()) {
             int cur = deque.pollFirst();
 
-            if (Order[cur - 1] == 0) {
-                Order[cur - 1] = Cnt++;
+            if (Visited[cur - 1] == 0) {
+                Visited[cur - 1] = 1;
+                Order[cur - 1] = cnt++;
 
                 for (int i : Graph[cur]) {
                     deque.addLast(i);
@@ -58,4 +61,6 @@ public class Main {
             }
         }
     }
+
+
 }
